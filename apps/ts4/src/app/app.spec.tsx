2f87569742209558c0d5db7c18f23a17ab3deec2
v0xvigneshwaran <vigneshwaran@vezham.com>
2025-08-03 02:52:25 +0530

    chore: renamed pkg names

--- apps/ts4/src/app/app.spec.tsx
+++ apps/ts4/src/app/app.spec.tsx
@@ -20,7 +20,7 @@ describe('App', () => {
       </BrowserRouter>
     )
     expect(
-      getAllByText(new RegExp('Welcome @v1xfe-kit/ts4', 'gi')).length > 0
+      getAllByText(new RegExp('Welcome @vx-fe-kit/ts4', 'gi')).length > 0
     ).toBeTruthy()
   })
 })
