4e467915fbb479398e4c3aac05813d08efa2cf31
v0xkrishnaprasad <krishnaprasad@vezham.com>
2025-09-05 03:55:19 +0530

    fix: books panel widgets align fix

--- apps/books/src/pages/books/overview/index.tsx
+++ apps/books/src/pages/books/overview/index.tsx
@@ -284,7 +284,7 @@ export default function Component(props: CardProps) {
   )
 
   return (
-    <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-1">
+    <div className={cv.gridContainer}>
       {isSalesLoading
         ? renderSkeleton()
         : isSalesError
--- apps/books/src/pages/books/overview/variant.ts
+++ apps/books/src/pages/books/overview/variant.ts
@@ -1,37 +1,56 @@
+// export const cardVariants = {
+//   wrapper:
+//     'border-small border-default-200 min-h-[255px] flex-1 p-3 shadow-none',
+//   body: 'pb-1',
+//   titleWrapper: 'flex items-center justify-between',
+//   title: 'text-lg leading-7 font-bold',
+//   menuIcon: 'text-small rotate-90 text-gray-400',
+//   gridWrapper: 'flex justify-between',
+//   gridLabels: 'grid gap-2',
+//   gridValues: 'grid gap-2',
+//   gridValues: 'grid gap-2 text-end',
+//   paid: 'text-success text-base leading-6 font-medium',
+//   outstanding: 'text-danger text-base leading-6 font-medium',
+//   normal: 'text-base leading-6 font-medium',
+//   footer: 'border-default-200 items-center justify-between border-t',
+//   footer: 'border-default-200 items-center justify-between border-t text-end',
+//   footerLeft: 'flex items-center gap-2'
+// }
+
 export const cardVariants = {
   wrapper: `
     border-small border-default-200 shadow-none p-3 gap-4
-    /* Default (xs < 400px) */
-   w-[164px] h-[164px]
-
-    /* sm (sm < md) */
-    sm:w-[300px] sm:h-[164px]
+    size-52
 
     /* md (≥768px) */
-    md:w-[300px] md:h-[300px]
+    sm:w-[300px] md:h-[300px]
 
     /* lg (≥1024px) */
     lg:w-[628px] lg:h-[300px]
   `,
   alertWrapper: `
     flex flex-col items-center
-
-    /* Default (xs < 400px) */
-   w-[164px] h-[164px]
-
-    /* sm (401px < md) */
-    sm:w-[300px] sm:h-[164px]
+  size-52
 
     /* md (≥768px) */
-    md:w-[300px] md:h-[300px]
+    sm:w-[300px] md:h-[300px]
 
     /* lg (≥1024px) */
     lg:w-[628px] lg:h-[300px]
   `,
+  gridContainer: `
+  mt-6 gap-4 flex flex-col flex-wrap sm:flex-nowrap
+
+  /* md (≥768px): row */
+  sm:flex-row
+  /* lg (≥1024px): stack again */
+  lg:flex-col
+`,
+
   body: 'pb-1 h-full',
   titleWrapper: 'flex items-center justify-between',
   title: 'text-lg leading-7 font-bold',
-  menuIcon: 'text-small rotate-90 text-gray-400',
+  menuIcon: 'text-small  text-gray-400',
   gridWrapper: 'flex justify-between mt-4',
   gridLabels: 'grid gap-2',
   gridValues: 'grid gap-2 text-end',
