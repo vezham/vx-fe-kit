9f185b6ecc9b709341316262f0986a475303cffa
v0xkrishnaprasad <krishnaprasad@vezham.com>
2026-04-18 18:51:18 +0530

    chore: apps school-os folder dnd code

--- apps/school/src/components/panel/header/bookmarks/SortableFolderItem.tsx
+++ apps/school/src/components/panel/header/bookmarks/SortableFolderItem.tsx
@@ -179,6 +179,92 @@ import { Accordion } from '@vezham/react/v3'
 //     )
 // }
 
+// 'use client'
+
+// import React, { useState } from 'react'
+// import { useSortable } from '@dnd-kit/sortable'
+// import { CSS } from '@dnd-kit/utilities'
+// import { Accordion } from '@vezham/react/v3'
+// import { Icon } from '@iconify/react'
+
+// interface SortableFolderItemProps {
+//     id: string
+//     folder: string
+//     items: any[]
+//     getFolderAccordionProps: any
+//     getFolderItemProps: any
+//     getFolderHeadingProps: any
+//     getFolderTriggerProps: any
+//     getFolderTriggerContentProps: any
+//     getFolderIconProps: any
+//     getFolderNameProps: any
+//     getFolderCountProps: any
+//     getFolderIndicatorProps: any
+//     getFolderPanelProps: any
+//     getFolderBodyProps: any
+//     renderBookmarkItems: (items: any[], isFolderContent: boolean) => React.ReactNode
+// }
+
+// export const SortableFolderItem: React.FC<SortableFolderItemProps> = ({
+//     id,
+//     folder,
+//     items,
+//     getFolderAccordionProps,
+//     getFolderItemProps,
+//     getFolderHeadingProps,
+//     getFolderTriggerProps,
+//     getFolderTriggerContentProps,
+//     getFolderIconProps,
+//     getFolderNameProps,
+//     getFolderCountProps,
+//     getFolderIndicatorProps,
+//     getFolderPanelProps,
+//     getFolderBodyProps,
+//     renderBookmarkItems,
+// }) => {
+//     const {
+//         attributes,
+//         listeners,
+//         setNodeRef,
+//         transform,
+//         transition,
+//         isDragging,
+//     } = useSortable({ id })
+
+//     const style = {
+//         transform: CSS.Transform.toString(transform),
+//         transition,
+//         opacity: isDragging ? 0.5 : 1,
+//         cursor: 'grab',
+//     }
+
+//     return (
+//         <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+//             <Accordion hideSeparator {...getFolderAccordionProps()}>
+//                 <Accordion.Item {...getFolderItemProps()}>
+//                     <Accordion.Heading {...getFolderHeadingProps()}>
+//                         <Accordion.Trigger {...getFolderTriggerProps()}>
+//                             <div {...getFolderTriggerContentProps()}>
+//                                 <Icon {...getFolderIconProps()} />
+//                                 <span {...getFolderNameProps(folder)} />
+//                                 <span {...getFolderCountProps(items.length)} />
+//                             </div>
+//                             <Accordion.Indicator {...getFolderIndicatorProps()}>
+//                                 <Icon icon="solar:alt-arrow-down-linear" width={18} />
+//                             </Accordion.Indicator>
+//                         </Accordion.Trigger>
+//                     </Accordion.Heading>
+//                     <Accordion.Panel {...getFolderPanelProps()}>
+//                         <Accordion.Body {...getFolderBodyProps()}>
+//                             {renderBookmarkItems(items, true)}
+//                         </Accordion.Body>
+//                     </Accordion.Panel>
+//                 </Accordion.Item>
+//             </Accordion>
+//         </div>
+//     )
+// }
+
 interface SortableFolderItemProps {
   id: string
   folder: string
